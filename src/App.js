a77d6ae4f6ed348d9ca72796572b0d06a046bd9c
Smilyan Pavlov <alexandra.tnicolau@gmail.com>
2019-03-08 10:33:44 +0000

    almost finished but some things dont work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import Note from "./Components/index";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      noteText: ""
+      noteText: "",
+      notes: []
     };
   }
 
@@ -13,11 +15,45 @@ class App extends Component {
     this.setState({ noteText: noteText.target.value });
   }
 
+  addNote() {
+    if (this.state.noteText === "") {
+      return;
+    }
+    let notesArr = this.state.notes;
+    notesArr.push(this.state.noteText);
+    this.setState({ noteText: "" });
+    this.textInput.focus();
+  }
+
+  handleKeyPress = event => {
+    if (event.key === "Enter") {
+    }
+  };
+
+  deleteNote(index) {
+    let notesArr = this.state.notes;
+    notesArr.splice(index, 1);
+    this.setState({ notes: notesArr });
+  }
+
   render() {
+    let notes = this.state.notes.map((value, key) => {
+      return (
+        <Note
+          key={key}
+          text={value}
+          deleteMethod={() => this.deleteNote(key)}
+        />
+      );
+    });
+
     return (
       <div className="Container">
         <div className="header"> React to Do application></div>
-        <div className="button">+</div>
+        {notes}
+        <div className="button" onClick={this.addNote.bind(this)}>
+          +
+        </div>
         <input
           type="text"
           ref={input => {
@@ -25,7 +61,7 @@ class App extends Component {
           }}
           className="textInput"
           value={this.state.noteText}
-          onChangeText={noteText => this.updateNoteeText(noteText)}
+          onChange={noteText => this.updateNoteeText(noteText)}
           onKeyPress={this.handleKeyPress.bind(this)}
         />
       </div>
